ee766285d6e92490860dee844916c3eb9d3d8cf1
katarilp <katarilp@metropolia.fi>
2025-01-22 15:27:48 +0200

    tunnilla week2

---
+++ src/items.js
@@ -0,0 +1,11 @@
+const items =
+  [{ "id": 1, "name":  "Omena" },
+  { "id": 2, "name": "Banaani" },
+  { "id": 3, "name": "Kiivi" }
+  { "id": 4, "name": "Appelsiini" }];
+
+  const getItems = (req, res) => {
+    res.json(items);
+  };
+
+  export{getItems}
