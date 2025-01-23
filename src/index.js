e9f20fa90e82804bf0f0f47ac2a5fcd4fd63a130
katarilp <katarilp@metropolia.fi>
2025-01-23 14:54:41 +0200

    14.54

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getItems, addItem} from './items.js';
+import {getItems, addItem, getItemById} from './items.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
